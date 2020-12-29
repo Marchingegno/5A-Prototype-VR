9746ac23cda7ee9003fd57d38e86af3119f06cec
Marchingegno <matteo.marchisciana@mail.polimi.it>
2020-12-29 12:44:44 +0100

    Add VolumeController.cs 2 part

---
+++ Assets/VolumeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] private GameObject schermataVolume;
+    private static VolumeController instance;
+
+    private AudioSource gameSoundsSource;
+    private AudioSource dialoguesSource;
+
+    private float mainVolume;
+    private float gameSoundsVolume;
+    
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            mainVolume = 0.5f;
+            gameSoundsVolume = 0.5f;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static VolumeController GetInstance()
+    {
+        return instance;
+    }
+
+    public void OnSceneChange()
+    {
+        gameSoundsSource = GameObject.FindGameObjectWithTag("GameSoundsSource").GetComponent<AudioSource>();
+        dialoguesSource = GameObject.FindGameObjectWithTag("DialoguesSource").GetComponent<AudioSource>();
+
+        dialoguesSource.volume = mainVolume;
+        gameSoundsSource.volume = mainVolume * gameSoundsVolume;
+
+
+
+    }
+    
+}
